4e43d555a1dfe15fe609fbe4d739647ba7f6b4d9
erik-farmer <erik@lildevbooksMBP.attlocal.net>
2024-06-25 21:05:26 -0700

    Add sqlite connection via turso.
    Turso token required in env var.
    ToDo: Move db to it's own module

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/tursodatabase/go-libsql"
 )
 
+func init_db() *sql.DB {
+	dbName := "local.db"
+	dbUrl := "libsql://me-and-u-dev-efdev.turso.io"
+	authToken := os.Getenv("TURSO_TOKEN")
+
+	dir, err := os.MkdirTemp("", "libsql-*")
+    if err != nil {
+        fmt.Println("Error creating temporary directory:", err)
+        os.Exit(1)
+    }
+    defer os.RemoveAll(dir)
+
+    dbPath := filepath.Join(dir, dbName)
+
+    connector, err := libsql.NewEmbeddedReplicaConnector(dbPath, dbUrl,
+        libsql.WithAuthToken(authToken),
+    )
+    if err != nil {
+        fmt.Println("Error creating connector:", err)
+        os.Exit(1)
+    }
+
+    db := sql.OpenDB(connector)
+    return db
+}
+
 func main() {
+    db := init_db()
+    defer db.Close()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
