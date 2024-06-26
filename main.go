bb387c7976110d0c092e35447c20bed8b212b9e3
erik-farmer <erik@lildevbooksMBP.attlocal.net>
2024-06-26 12:26:53 -0700

    Fix trailing slashes :)

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ func main() {
 	})
 
 
-	mux.HandleFunc("GET /recipes", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /recipes/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/recipe_list.html"))
 		tmpl.Execute(w, nil)
 	})
 
-	mux.HandleFunc("GET /recipe/new", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /recipe/new/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/new_recipe.html"))
 		tmpl.Execute(w, nil)
 	})
