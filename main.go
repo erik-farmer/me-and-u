4abb48e296dd5405abfb07281b825ad2706e3e4c
erik-farmer <erik@lildevbooksMBP.attlocal.net>
2024-07-02 12:56:09 -0700

    Small changes to routes, adds saving data to db

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
+	"github.com/erik-farmer/me-and-u/data"
 	"github.com/erik-farmer/me-and-u/db"
 )
 
 func main() {
-	db := db.InitDB()
-	defer db.Close()
+	dir := db.MakeDbDir()
+	defer os.RemoveAll(dir)
+
+	connector := db.MakeDbConnector(dir)
+	defer connector.Close()
+
+	// ToDo: https://www.alexedwards.net/blog/organising-database-access
+	// Method 2 is probably fine and good practice
+	database := sql.OpenDB(connector)
+	defer database.Close()
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World! you have hit the root")
-	})
+	// ToDo: Add a router? Maybe if they start to get complicated
+	// ToDo: make handlers it's own package?
 
-	mux.HandleFunc("GET /recipes/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/recipe_list.html"))
 		tmpl.Execute(w, nil)
 	})
@@ -31,7 +41,21 @@ func main() {
 	mux.HandleFunc("POST /recipe/new/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Validating that we hit POST")
 		r.ParseForm()
-		fmt.Printf("NAME => %s\n", r.FormValue("name"))
+
+		recipe := data.Recipe{
+			Name:        r.FormValue("name"),
+			URL:         r.FormValue("url"),
+			Ingredients: r.FormValue("ingredients"),
+			Steps:       r.FormValue("steps"),
+			Notes:       r.FormValue("notes"),
+		}
+
+		stmt := "INSERT INTO recipes (name, url, ingredients, steps, notes) VALUES(?,?,?,?,?);"
+		_, err := database.Exec(stmt, recipe.Name, recipe.URL, recipe.Ingredients, recipe.Steps, recipe.Notes)
+		if err != nil {
+			fmt.Printf("There was an error executing: \n%s\n", stmt)
+			fmt.Printf("Error: \n%s\n", err)
+		}
 
 		tmpl := template.Must(template.ParseFiles("templates/new_recipe.html"))
 		tmpl.Execute(w, nil)
